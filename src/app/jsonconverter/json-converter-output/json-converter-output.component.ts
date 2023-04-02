b9f424cb656491440250eb73bb82c741aa115f15
Zishan <zishanmohammed02@gmail.com>
2023-04-03 00:51:58 +0530

    moving changes for input

--- src/app/jsonconverter/json-converter-output/json-converter-output.component.ts
+++ src/app/jsonconverter/json-converter-output/json-converter-output.component.ts
@@ -7,7 +7,7 @@ import { Component, Input } from '@angular/core';
 })
 export class JsonConverterOutputComponent {
 
-  @Input() temp :string ='';
+  @Input() temp :String ='';
   @Input() result:string='';
 
 }
